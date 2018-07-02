eb82caafd0ca3d001a0acb4d0af1ffec58d825ad
Chinmaya Panda <chinmaya_panda@yahoo.com>
2018-07-02 14:46:17 +0530

    added bytes range

--- grok/utils.py
+++ grok/utils.py
@@ -1,10 +1,13 @@
 import requests
 import json
 import os
+from itertools import zip_longest
 
 class Downloader:
     def __init__(self, url):
         self.url = url
+        self.chunksize = 1024 * 1024
+        self.length = 104857610
         
     def raw_res(self):
         res = requests.get(self.url, stream=True)
@@ -13,3 +16,8 @@ class Downloader:
     def download(self):
         pass
         
+    def _bytes_ranges(self):
+        starts = range(0,self.length,self.chunksize)
+        ends = range(self.chunksize-1,self.length,self.chunksize)
+        ranges = [{'Range': '{}-{}'.format(s,e)} for s,e in zip_longest(starts,ends,fillvalue=self.length-1)]
+        return ranges
