eda60619179c077a052207c4acab33a84a96aad6
Chinmaya Panda <chinmaya_panda@yahoo.com>
2018-07-02 14:08:12 +0530

    added raw_res function

--- grok/utils.py
+++ grok/utils.py
@@ -5,3 +5,11 @@ import os
 class Downloader:
     def __init__(self, url):
         self.url = url
+        
+    def raw_res(self):
+        res = requests.get(self.url, stream=True)
+        return res
+
+    def download(self):
+        pass
+        
